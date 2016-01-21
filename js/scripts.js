3e6952eef61316ab48857cd19ce2dedfe96729c4
Daren Schaad and Tyler Suderman <tcsuder@gmail.com>
2016-01-21 14:16:25 -0800

    Adds logic to return results in an unordered list on the web page

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,24 @@
-
 var multiplier = function(countBy, countTo) {
   var array = [];
   for (var i = countBy; i <= countTo; i = i + countBy){
   array.push(i);
   }
-  return array.toString();
+  return array;
 };
-
-
-
-
 
 $(document).ready(function() {
   $("form#counter").submit(function(event) {
     var countBy = parseInt($("#countBy").val());
     var countTo = parseInt($("#countTo").val());
     var result = multiplier(countBy, countTo);
-    $(".result").text(result);
-    $("#hidden").show();
+    var listItem = ""
+
+    for (var i = 0; i < result.length; i++ ) {
+        listItem += "<li>"  + result[i] + "</li>";
+    }
+
+    document.getElementById("itemList").innerHTML = listItem;
+      $("#hidden").show();
     event.preventDefault();
   });
 });
